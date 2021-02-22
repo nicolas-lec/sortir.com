ecb702b4e4e6d37e6a85838934b9b12eefd97333
joa-tech <joachim.gaignard2020@campus-eni.fr>
2021-02-22 16:21:18 +0100

    Form

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\File;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('id', IntegerType::class, [
+                'label' => 'ID',
+                'attr' =>
+                    [
+                        'readonly' => true
+                    ]
+            ])
+            ->add('pseudo', TextType::class)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('telephone', TextType::class)
+            ->add('mail', TextType::class)
+            ->add('motDePasse', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les deux mots de passes doivent correspondre.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+            ])
+
+        ->add('actif', CheckboxType::class, [
+        'label' => 'Actif',
+        'required' => false
+        ])
+        ->add('submit',SubmitType::class, [
+            'label' => 'Soumettre',
+            'attr' => [
+                'class' => 'btn btn-success w-100'
+            ]
+        ])
+    ;
+    }
+
+}
