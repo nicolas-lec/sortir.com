7d5c88bc7e6ab63e7e12d09de18a1b2401e837b5
Nicolas <nicolas.leclerc2020@campus-eni.fr>
2021-02-23 11:55:39 +0100

    formulaire_inscription_admin

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Participant;
 use App\Form\ParticipantType;
+use App\Security\LoginAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 /**
  * @Route (name="participant_", path="participant/")
@@ -15,21 +19,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class ParticipantController extends AbstractController
 {
     /**
-     * @Route(path="/register-user", name="registerUser", methods={"GET","POST"})
+     * @Route(path="inscription", name="inscription", methods={"GET","POST"})
      */
-    public function inscription(EntityManagerInterface $entityManager): Response
+    public function inscription(Request $request, EntityManagerInterface $entityManager, LoginAuthenticator $login, GuardAuthenticatorHandler $guard, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $participant = new Participant();
-        $form = $this->createForm(ParticipantType::class);
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
 
-        if  ($form->isSubmitted() && $form->isValid()) {
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $participant->setPassword($passwordEncoder->encodePassword($participant, $participant->getPlainPassword()));
+
+            $participant->setRoles(['ROLE_USER']);
+
             $entityManager->persist($participant);
             $entityManager->flush();
+
+            $this->addFlash('success', 'L\'inscritpion a été un succès ! ;)');
+
+
+            return $guard->authenticateUserAndHandleSuccess($participant, $request, $login, 'app_user_provider' );
         }
-        return $this->render('participant/register.html.twig',
-            ['participantForm' => $form->createView()]
-        );
+
+        return $this->render('participant/register.html.twig', ['participantForm' => $form->createView()]);
     }
+
     /**
      * @Route("/profil", name="profil")
      */
