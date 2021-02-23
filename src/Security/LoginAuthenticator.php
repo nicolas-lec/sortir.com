451b3a71a55ad8871589f5e6197abfb5c2749b37
yannringard <yannvincent.ringard@gmail.com>
2021-02-23 08:56:54 +0100

    Add fixture

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -96,7 +96,7 @@ class LoginAuthenticator extends AbstractFormLoginAuthenticator implements Passw
             return new RedirectResponse($targetPath);
         }
 
-     return new RedirectResponse($this->urlGenerator->generate('home'));
+     return new RedirectResponse($this->urlGenerator->generate('home_home'));
 
     }
 
