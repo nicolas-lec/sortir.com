86530ce48bd2c67fb736a499e64db997efa944e2
Nicolas <nicolas.leclerc2020@campus-eni.fr>
2021-02-24 15:04:37 +0100

    Condition désinscription

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\DataFixtures;
-
+use App\Entity\Etat;
 use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
 class UserFixtures extends Fixture
 {
     private $encoder;
-
     public function __construct(UserPasswordEncoderInterface $encoder)
     {
         $this->encoder = $encoder;
@@ -34,10 +30,40 @@ class UserFixtures extends Fixture
             $participant->setPassword($password);
             $manager->persist($participant);
         }
-
+        $etat1 = new Etat();
+        $etat1 ->setLibelle('Publiée');
+        $manager->persist($etat1);
+        $etat2 = new Etat();
+        $etat2 ->setLibelle('En cours');
+        $manager->persist($etat2);
+        $etat3= new Etat();
+        $etat3->setLibelle('Annulé');
+        $manager->persist($etat3);
+        // $product = new Product();
+        // $manager->persist($product);
+        for($i=0; $i<20; $i++) {
+            $sortie = new Sortie();
+            $sortie ->setNom('Sortie '.$i);
+            $sortie ->setDateHeureDebut(\DateTime::createFromFormat('Y-m-d H:i:s', '2021-02-24 19:30:00'));
+            $sortie ->setDuree(60+$i);
+            $sortie ->setDateLimiteInscription(\DateTime::createFromFormat('Y-m-d H:i:s', '2021-02-28 19:30:00'));
+            $sortie ->setNbInscriptionsMax(10+$i);
+            $sortie->setInfoSortie('Sortie ENI n°'.$i);
+            $sortie->setEtat($etat1);
+            $manager->persist($sortie);
+        }
+        for($i=0; $i<20; $i++) {
+            $sortie = new Sortie();
+            $sortie ->setNom('Sortie '.$i);
+            $sortie ->setDateHeureDebut(\DateTime::createFromFormat('Y-m-d H:i:s', '2021-02-24 19:30:00'));
+            $sortie ->setDuree(60+$i);
+            $sortie ->setDateLimiteInscription(\DateTime::createFromFormat('Y-m-d H:i:s', '2021-02-28 19:30:00'));
+            $sortie ->setNbInscriptionsMax(10+$i);
+            $sortie->setInfoSortie('Sortie ENI n°'.$i);
+            $sortie->setEtat($etat2);
+            $manager->persist($sortie);
+        }
         $manager->flush();
     }
-
 }
-
 
