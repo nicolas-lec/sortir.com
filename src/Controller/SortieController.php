421bf109018ab6ed5265d1319a617f2ee1faf25f
joa-tech <joachim.gaignard2020@campus-eni.fr>
2021-02-24 16:55:30 +0100

    date

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -112,7 +112,7 @@ class SortieController extends AbstractController
         $participant = $this->getUser();
 
 
-        if($sortie->getDateLimiteInscription() > $sortie-> getDateHeureDebut()){
+
 
 
         if ($sortie->getEtat()->getId()===1) {
@@ -131,9 +131,7 @@ class SortieController extends AbstractController
             // Ajout d'un message de confirmation
             $this->addFlash('warning', 'Vous ne pouvez pas vous inscrire à la sortie !');
         }
-        }else{
-            $this -> addFlash('warning','Vous ne pouvez pas vous inscrire à la sortie !');
-        }
+
 
         return $this->render('sortie/detailSortie.html.twig', ['sortie'=>$sortie]);
     }
