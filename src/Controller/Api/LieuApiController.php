6bd245b0b18014470f84ed8f952beeaf1b17b052
joa-tech <joachim.gaignard2020@campus-eni.fr>
2021-03-02 10:43:42 +0100

    modifier une sortie avec api

--- src/Controller/Api/LieuApiController.php
+++ src/Controller/Api/LieuApiController.php
@@ -14,14 +14,13 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class LieuApiController extends AbstractController
 {
     /**
-     * @Route(path="/api/v1/lieu/{id}", name="api_lieu_api", methods={"POST", "GET"})
+     * @Route(path="/api/v1/lieu/{id}", name="api_lieu_api", methods={"GET"})
      * @param LieuRepository $repository
      * @param SerializerInterface $serializer
      * @return Response
      */
-    public function listLieu(Request $request,LieuRepository $repository, SerializerInterface $serializer):Response{
+    public function listLieu($id,Request $request,LieuRepository $repository, SerializerInterface $serializer):Response{
         if($request -> isXmlHttpRequest()){
-            $id = $request->request->get('id');
             $lieu = $repository->find((int)$id);
             $json = $serializer->serialize($lieu,'json',['groups'=>'list_lieu']);
             return new JsonResponse($json,200,[],true);
