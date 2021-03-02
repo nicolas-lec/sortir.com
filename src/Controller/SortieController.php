c5797ffd702e8fc58ca76c1d8f09d1ed0589a100
JG <joachim.gaignard2020@campus-eni.fr>
2021-03-02 18:07:22 +0100

    Modifier une sortie et c'est tout bon

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -255,13 +255,20 @@ class SortieController extends AbstractController
             }
             $env = $request->request->get('modifier');
             $publier = 'publier';
-
+            $enregistrer = 'enregistrer';
             if($env === $publier){
                 $etat = $em->getRepository('App:Etat')->findOneBy(['id'=>1]);
                 $sortie->setEtat($etat);
                 // Ajout d'un message de confirmation
-                $this->addFlash('success', 'Votre sortie a été publier avec succès !');
+                $this->addFlash('success', 'Votre sortie a été modifiée et publier avec succès !');
+            }elseif($env === $enregistrer){
+                $etat = $em->getRepository('App:Etat')->findOneBy(['id'=>2]);
+                $sortie->setEtat($etat);
+                // Ajout d'un message de confirmation
+                $this->addFlash('success', 'Votre sortie a été modifiée et mis en brouillons avec succès !');
             }
+            $em->persist($sortie);
+            $em->flush();
             return $this->redirectToRoute("sortie_detailSortie",
                 ['id' => $sortie->getId()]);
         }
