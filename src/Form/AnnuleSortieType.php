9f894f08bb7d8e2a3846179c8acbc174e95548fb
yannringard <yannvincent.ringard@gmail.com>
2021-03-04 11:42:46 +0100

    Bootstrap

--- src/Form/AnnuleSortieType.php
+++ src/Form/AnnuleSortieType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,10 @@ class AnnuleSortieType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('descriptionAnnul')
+            ->add('descriptionAnnul', TextType ::class, [
+                'label' => ' Indiquez un motif d\'annulation : ',
+            ]);
+
         ;
         $builder->add('submit', SubmitType::class, [
             'label' => 'Envoyer',
